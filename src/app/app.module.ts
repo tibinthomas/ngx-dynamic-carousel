62497dc48ce3a31ad79551f1d16945361024b0dd
tthomas <tibinthomas@outlook.com>
2021-05-15 15:26:26 +0530

    import lib into app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { NgxDynamicCarouselModule } from 'ngx-dynamic-carousel';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxDynamicCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
