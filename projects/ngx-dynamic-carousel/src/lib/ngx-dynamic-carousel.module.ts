ce0371c4fe76e5e99b8f94b247676ed847ea55ff
tthomas <tibinthomas@outlook.com>
2021-05-15 15:07:59 +0530

    add lib project

---
+++ projects/ngx-dynamic-carousel/src/lib/ngx-dynamic-carousel.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgxDynamicCarouselComponent } from './ngx-dynamic-carousel.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgxDynamicCarouselComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgxDynamicCarouselComponent
+  ]
+})
+export class NgxDynamicCarouselModule { }
