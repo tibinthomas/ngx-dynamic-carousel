f5d7157d079c7feb6275720e7c35c83b05670d33
Fenn Saji <fennig1997@gmail.com>
2021-05-15 15:14:06 +0530

    Changed app name

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/ngx-dynamic-carousel'),
+      dir: require('path').join(__dirname, './coverage/ngx-dynamic-carousel-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngx-dynamic-carousel'`, () => {
+  it(`should have as title 'ngx-dynamic-carousel-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngx-dynamic-carousel');
+    expect(app.title).toEqual('ngx-dynamic-carousel-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ngx-dynamic-carousel app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ngx-dynamic-carousel-app app is running!');
   });
 });
