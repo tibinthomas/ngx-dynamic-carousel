fd4aa7867903a7b6f5bdece5f888061ccafbee97
Fenn Saji <fennig1997@gmail.com>
2021-05-16 15:37:22 +0530

    Fade in fade out in progress

--- projects/ngx-dynamic-carousel/src/lib/ngx-dynamic-carousel.component.ts
+++ projects/ngx-dynamic-carousel/src/lib/ngx-dynamic-carousel.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit, SimpleChanges, TemplateRef } from '@angular/core';
 
-import { animate, keyframes, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'ngx-dynamic-carousel',
@@ -16,11 +16,15 @@ import { animate, keyframes, style, transition, trigger } from '@angular/animati
   animations: [
     trigger('cardAnimator', [
       transition(':enter', [
-        style({opacity: 0}),
-        animate('1000ms ease-in', style({opacity: 1}))
+        style({display: 'none'}),
+        animate('10000ms ease-in', keyframes([
+          style({display: 'block', offset: 0.5}),
+          style({opacity: 0, offset: 0.5}),
+          style({opacity: 1, offset: 1})
+        ])),
       ]),
       transition(':leave', [
-        animate('1000ms ease-in', style({opacity: 0}))
+        animate('5000ms ease-out', style({opacity: 0}))
       ])
     ])
   ]
